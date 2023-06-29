801d856786fc196b4b35de5b27d39f96bbc01cca
rowan-nicetouch <rowan@nicetouch.co>
2023-06-29 00:03:40 -0700

    Install and configure cypress testing library.

---
+++ cypress/component/FirstTimeTesting.cy.js
@@ -0,0 +1,9 @@
+import readwpPostMetadata from 'base/readers/readWpPostMetadata'
+
+describe('FirstTimeTesting.cy.js', () => {
+  it('playground', () => {
+    // cy.mount()
+
+    console.log(readwpPostMetadata())
+  })
+})
