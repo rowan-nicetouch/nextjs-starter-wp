b1a44226f1fbe2dd0b5344700acfe07376bee820
rowan-nicetouch <rowan@nicetouch.co>
2023-07-01 05:21:09 -0700

    Enhance readDataUrl
      - must check for string before calling string functions.
      - return sanitized URL via cleanUrl().

--- src/base/read/readDataUrl.js
+++ src/base/read/readDataUrl.js
@@ -1,14 +1,22 @@
-export function readDataUrl (url) {
+import cleanUrl from 'base/clean/cleanUrl'
+
+export default function readDataUrl (url) {
   const search = process.env.NEXT_PUBLIC_WP_URL
   const replace = process.env.NEXT_PUBLIC_URL
 
-  if (search.slice(-1) === '/' && replace.slice(-1) === '/') {
-    return url.replaceAll(search, replace)
-  } else if (search.slice(-1) === '/') {
-    return url.replaceAll(search, replace + '/')
-  } else if (replace.slice(-1) === '/') {
-    return url.replaceAll(search + '/', replace)
-  } else {
-    return url.replaceAll(search + '/', replace + '/')
-  }
+  const replaced = (() => {
+    if (typeof url !== 'string') {
+      return url
+    } else if (search.slice(-1) === '/' && replace.slice(-1) === '/') {
+      return url.replaceAll(search, replace)
+    } else if (search.slice(-1) === '/') {
+      return url.replaceAll(search, replace + '/')
+    } else if (replace.slice(-1) === '/') {
+      return url.replaceAll(search + '/', replace)
+    } else {
+      return url.replaceAll(search + '/', replace + '/')
+    }
+  })()
+
+  return cleanUrl(replaced)
 }
