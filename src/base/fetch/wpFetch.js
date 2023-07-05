2146052bf19f685a813552014e7417e7edb8e874
rowan-nicetouch <rowan@nicetouch.co>
2023-07-05 01:28:28 -0700

    Add base wpFetch() wrapper.

---
+++ src/base/fetch/wpFetch.js
@@ -0,0 +1,30 @@
+import _getWpApiUrl from 'base/fetch/_getWpApiUrl'
+
+/**
+ * Fetch from WordPress REST API v2
+ *
+ * @param {String} endpoint - (required)
+ * @param {Mixed} urlParams - (optional) Any value that can safely be passed as
+ *   parameter one to the URLSearchParams constructor.
+ * @param {Object} fetchOptions - (optional) Passed as parameter 2 to fetch().
+ *
+ * @return {Promise}
+ */
+export default async function wpFetch (endpoint, urlParams, fetchOptions) {
+  fetchOptions = (() => {
+    // Ensure that a revalidation strategy is always present.
+    const output = {
+      revalidate: 10
+    }
+    if (typeof fetchOptions === 'object') {
+      return {...output, ...fetchOptions}
+    } else {
+      return output
+    }
+  })()
+
+  const resource = _getWpApiUrl(endpoint, urlParams)
+  const response = await fetch(resource, fetchOptions)
+
+  return response
+}
