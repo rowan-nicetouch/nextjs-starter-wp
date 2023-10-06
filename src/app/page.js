bef23a54dee3b7148192644da83869e2832fb0d8
Rowan <rowan@nicetouch.co>
2023-10-06 12:17:17 -0700

    Introduce a simple page.js file.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import { wpGetPages } from 'base/fetch/wpGetPages'
+import { notFound } from 'next/navigation'
+
+export default async function Home () {
+
+  const page = await getPage()
   return (
     <main>
 
     </main>
   )
+}
+
+async function getPage () {
+  try {
+    const pages = await wpGetPages({
+      slug: 'home',
+      _embed: true,
+    }, {
+      cache: 'no-cache'
+    })
+    console.dir(pages[0], { depth: null })
+    return pages[0]
+  } catch (error) {
+    console.error(error)
+    notFound()
+  }
 }
